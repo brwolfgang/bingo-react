fe3d351e7ed138407fae95b89b80e62c8f948398
Wolfgang Marcos <wmarcos@sefaz.am.gov.br>
2021-12-31 17:45:24 -0300

    Ajusta detalhes do botão de ação de sorteio

--- src/componentes/AcaoSorteio.jsx
+++ src/componentes/AcaoSorteio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AcaoSorteio = ({sortearNumero}) => {
     return (
-        <button className="text-white bg-teal-500 hover:bg-teal-700 px-2 py-1 rounded-md" onClick={sortearNumero}>Sortear Número!</button>
+        <button className="text-white bg-teal-600 hover:bg-teal-700 px-2 py-1 rounded-md shadow" onClick={sortearNumero}>Sortear Número!</button>
     )
 }
 
