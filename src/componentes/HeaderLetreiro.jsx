8fde2e8f33c398dc5ba597d96e315aa78173af51
Wolfgang Marcos <wmarcos@sefaz.am.gov.br>
2021-12-31 17:45:40 -0300

    Migra letreiro

--- src/componentes/HeaderLetreiro.jsx
+++ src/componentes/HeaderLetreiro.jsx
@@ -7,11 +7,9 @@ const estiloLetra = {
 
 const HeaderLetreiro = (props) => {
     return (
-        <div className="col-2">
-            <div className="form-group">
-                <dt>Letra</dt>
-                <dd style={estiloLetra}>{props.letra.toUpperCase()}</dd>
-            </div>
+        <div className="flex flex-col flex-nowrap py-1 items-center">
+            <dt>Letra</dt>
+            <dd style={estiloLetra}>{props.letra.toUpperCase()}</dd>
         </div>
     )
 }
