899e516fcde16965f362d3fdca476cf5b968469e
Wolfgang Marcos <wmarcos@sefaz.am.gov.br>
2021-12-31 17:50:00 -0300

    Ajusta estilo do painel de número sorteado

--- src/componentes/NumeroSorteado.jsx
+++ src/componentes/NumeroSorteado.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const NumeroSorteado = ({numeroSorteado}) => {
     return (
-        <div className="flex flex-col border-teal-500 m-2 py-1 px-2 border-2 rounded-md">
-            <p className="text-3xl">Número sorteado:</p>
+        <div className="flex flex-col bg-yellow-400 m-2 py-1 px-2 rounded-md shadow">
+            <p className="text-3xl">Número Sorteado</p>
             <h1 className="text-4xl font-mono">{numeroSorteado.letra} {numeroSorteado.valor}</h1>
         </div>
     )
