bd2398f7aa1684318f3e1bc2b8d2300e7e4f3fc5
Wolfgang Marcos <wmarcos@sefaz.am.gov.br>
2021-12-31 16:43:07 -0300

    Remove marmota do jQuery

--- src/componentes/Numero.jsx
+++ src/componentes/Numero.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import jQuery from 'jquery'
 
 class Numero extends React.Component {
-        componentDidMount = () => {
-                jQuery('[data-toggle="tooltip"]').tooltip()
+    render() {
+        if (this.props.isSorteado) {
+            return (
+                <div className="text-monospace form-text font-weight-bold"
+                     title={`Este foi o ${this.props.ordem}º número sorteado`}>{this.props.valor} <i className="bi bi-check-circle-fill text-success"/></div>
+            )
+        } else {
+            return <div className="text-monospace form-text text-muted">{this.props.valor}</div>
         }
-
-        render() {
-                if (this.props.isSorteado) {
-                        return <div className="text-monospace form-text font-weight-bold" title={`Este foi o ${this.props.ordem}º número sorteado`} data-toggle="tooltip">{this.props.valor} <i className="bi bi-check-circle-fill text-success"></i></div>
-                } else {
-                        return <div className="text-monospace form-text text-muted">{this.props.valor}</div>
-                }
-        }
+    }
 }
 
 export default Numero;
