77ec5d284123900bb9fff172be469858449d1998
Wolfgang Marcos <3627736+brwolfgang@users.noreply.github.com>
2023-02-27 21:36:38 -0400

    Cria menu com as rotas de bingo e configuração

--- src/pages/RouteLayout.tsx
+++ src/pages/RouteLayout.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
-import {Outlet} from "react-router-dom";
+import {Outlet, Link} from "react-router-dom";
 
 class RouteLayout extends React.Component {
   render() {
     return (
-      <div className="flex flex-col justify-center items-center w-screen h-screen">
-        <div className="flex flex-col justify-center items-center m-2 text-center w-full">
+      <>
+        <div className="flex justify-around items-center">
+          <Link to="/">Bingo</Link>
+          <Link to="/config">Configurações</Link>
+        </div>
+      <div className="flex flex-col items-center w-screen h-screen">
+        <div className="flex flex-col items-center m-2 text-center w-full">
           <Outlet/>
         </div>
       </div>
+      </>
     );
   }
 }
