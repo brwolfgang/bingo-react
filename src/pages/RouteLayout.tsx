b6f39707f5a07f34dde5a7487be2d8b59d6b3701
Wolfgang Marcos <3627736+brwolfgang@users.noreply.github.com>
2023-03-07 21:07:11 -0300

    Implementa novo navbar usando template do tailwind

--- src/pages/RouteLayout.tsx
+++ src/pages/RouteLayout.tsx
@@ -5,15 +5,25 @@ class RouteLayout extends React.Component {
   render() {
     return (
       <>
-        <div className="flex justify-around items-center">
-          <Link to="/">Bingo</Link>
-          <Link to="/config">Configurações</Link>
+        <nav className="bg-gray-800">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    <Link to="/" className="bg-gray-600 hover:bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Bingo</Link>
+                    <Link to="/config" className="bg-gray-600 hover:bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Configurações</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center w-screen h-screen">
+          <div className="flex flex-col items-center m-2 text-center w-full">
+            <Outlet/>
+          </div>
         </div>
-      <div className="flex flex-col items-center w-screen h-screen">
-        <div className="flex flex-col items-center m-2 text-center w-full">
-          <Outlet/>
-        </div>
-      </div>
       </>
     );
   }
